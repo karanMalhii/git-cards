4504659499a197ac0adcc73b3edef66ce3062967
karanmalhii <karanmalhii1313@gmail.com>
2024-01-01 22:14:47 +0530

    fixed card issue

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-// import Button from 'react-bootstrap/Button';
-// import Card from 'react-bootstrap/Card';
-import { Button, Card } from "bootstrap";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+// import { Button, Card } from "bootstrap";
 
 export default function Data({ mydata }) {
   console.log({ mydata });
@@ -9,7 +9,7 @@ export default function Data({ mydata }) {
   return (
     <div>
       <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         <Card.Body>
           <Card.Title>Card Title</Card.Title>
           <Card.Text>
